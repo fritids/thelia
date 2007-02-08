5871ec9df591943090fc230707ed0b37b7bb1116
yoandm <yoandm@6d4e0a53-8826-0410-8364-8b493f4a4454>
2007-02-08 09:07:12 +0000

    git-svn-id: https://thelia.svn.sourceforge.net/svnroot/thelia@87 6d4e0a53-8826-0410-8364-8b493f4a4454

---
+++ thelia/fonctions/substitproduits.php
@@ -0,0 +1,62 @@
+<?php
+/*************************************************************************************/
+/*                                                                                   */
+/*      Thelia	                                                            		 */
+/*                                                                                   */
+/*      Copyright (c) Octolys Development		                                     */
+/*		email : thelia@octolys.fr		        	                             	 */
+/*      web : http://www.octolys.fr						   							 */
+/*                                                                                   */
+/*      This program is free software; you can redistribute it and/or modify         */
+/*      it under the terms of the GNU General Public License as published by         */
+/*      the Free Software Foundation; either version 2 of the License, or            */
+/*      (at your option) any later version.                                          */
+/*                                                                                   */
+/*      This program is distributed in the hope that it will be useful,              */
+/*      but WITHOUT ANY WARRANTY; without even the implied warranty of               */
+/*      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                */
+/*      GNU General Public License for more details.                                 */
+/*                                                                                   */
+/*      You should have received a copy of the GNU General Public License            */
+/*      along with this program; if not, write to the Free Software                  */
+/*      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA    */
+/*                                                                                   */
+/*************************************************************************************/
+?>
+<?php
+
+	include_once("classes/Produit.class.php");
+	include_once("classes/Produitdesc.class.php");
+	
+	/* Subsitutions de type produits */
+		
+	function substitproduits($texte){
+		global $ref, $reforig, $motcle, $id_produit, $classement, $prixmin, $prixmax;
+
+		
+		$tproduit = new Produit();
+		$tproduitdesc = new Produitdesc();
+		
+		$tproduit->charger($ref);
+
+
+		$tproduitdesc->charger($tproduit->id, $_SESSION['navig']->lang);
+		
+		if( $ref || $id_produit){        
+       	 	$texte = ereg_replace("#PRODUIT_REFORIG", "$reforig", $texte);
+			$texte = ereg_replace("#PRODUIT_REF", "$ref", $texte);
+			$texte = ereg_replace("#PRODUIT_ID", $tproduitdesc->produit, $texte);
+			$texte = ereg_replace("#PRODUIT_MOTCLE", "$motcle", $texte);
+			$texte = ereg_replace("#PRODUIT_NOM", $tproduitdesc->titre, $texte);
+			$texte = ereg_replace("#PRODUIT_RUBRIQUE", $tproduit->rubrique, $texte);
+		}
+		
+		$texte = ereg_replace("#PRODUIT_CLASSEMENT", "$classement", $texte);
+		$texte = ereg_replace("#PRODUIT_PRIXMIN", "$prixmin", $texte);
+		$texte = ereg_replace("#PRODUIT_PRIXMAX", "$prixmax", $texte);
+				
+		return $texte;
+	
+	}
+	
+?>
