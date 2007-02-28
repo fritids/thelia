b64154eb33a0cb9b276e2937c6bde50f00017601
yoandm <yoandm@6d4e0a53-8826-0410-8364-8b493f4a4454>
2007-02-28 17:25:45 +0000

    thelia/admin/message_modifier.php
    git-svn-id: https://thelia.svn.sourceforge.net/svnroot/thelia@145 6d4e0a53-8826-0410-8364-8b493f4a4454

---
+++ thelia/admin/variable_modifier.php
@@ -0,0 +1,49 @@
+<?php
+/*************************************************************************************/
+/*                                                                                   */
+/*      Thelia	                                                            		 */
+/*                                                                                   */
+/*      Copyright (c) Octolys Development		                                     */
+/*		email : thelia@octolys.fr		        	                             	 */
+/*      web : http://www.octolys.fr						   							 */
+/*                                                                                   */
+/*      This program is free software; you can redistribute it and/or modify         */
+/*      it under the terms of the GNU General Public License as published by         */
+/*      the Free Software Foundation; either version 2 of the License, or            */
+/*      (at your option) any later version.                                          */
+/*                                                                                   */
+/*      This program is distributed in the hope that it will be useful,              */
+/*      but WITHOUT ANY WARRANTY; without even the implied warranty of               */
+/*      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                */
+/*      GNU General Public License for more details.                                 */
+/*                                                                                   */
+/*      You should have received a copy of the GNU General Public License            */
+/*      along with this program; if not, write to the Free Software                  */
+/*      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA    */
+/*                                                                                   */
+/*************************************************************************************/
+?>
+<?php
+	include_once("pre.php");
+	include("auth.php");
+	
+	if(!isset($action)) $action="";
+	
+?>
+<?php
+	include("../classes/Variable.class.php");
+
+	if($action == "modifier"){
+	
+		$variable = new Variable();
+
+ 		$variable->charger($nom);
+ 		$variable->valeur = $valeur;	
+ 		$variable->maj();
+ 			
+		header("Location: variable.php");
+	}		
+	
+
+
+?>
