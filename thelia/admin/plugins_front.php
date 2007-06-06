0d755833a6f24d87fe1cdb510ca54dc0e5a90517
yoandm <yoandm@6d4e0a53-8826-0410-8364-8b493f4a4454>
2007-06-06 14:27:11 +0000

    thelia/admin/configuration.php
    git-svn-id: https://thelia.svn.sourceforge.net/svnroot/thelia@235 6d4e0a53-8826-0410-8364-8b493f4a4454

--- thelia/admin/plugins_front.php
+++ thelia/admin/plugins_front.php
@@ -38,63 +38,93 @@
 <body>
 
 <?php
-	include_once("../classes/Boutique.class.php");
+	include_once("../classes/Modules.class.php");
 ?>
 <?php
-	$menu="configuration";
+	$menu="paiement";
 	include_once("entete.php");
 ?>
 
 <div id="contenu_int"> 
-   <p class="titre_rubrique">Configuration </p>
-     <p align="right" class="geneva11Reg_3B4B5B"><a href="accueil.php" class="lien04">Accueil </a> <img src="gfx/suivant.gif" width="12" height="9" border="0" /><a href="#" class="lien04">Configuration</a>              
+   <p class="titre_rubrique">Gestion des modes de paiement </p>
+     <p align="right" class="geneva11Reg_3B4B5B"><a href="accueil.php" class="lien04">Accueil </a> <img src="gfx/suivant.gif" width="12" height="9" border="0" /><a href="paiement.php" class="lien04">Gestion de paiement</a>  <img src="gfx/suivant.gif" width="12" height="9" border="0" /> <a href="#" class="lien04">Gestion des modes de paiement</a>
     </p>
      <table width="710" border="0" cellpadding="5" cellspacing="0">
      <tr>
-       <td width="600" height="30" class="titre_cellule_tres_sombre">VOTRE CHOIX</td>
+       <td width="600" height="30" class="titre_cellule_tres_sombre">LISTE DES MODES DE PAIEMENT</td>
      </tr>
    </table>
+
+
+<?php
+
+		
+	$i=0;
+	
+	$d = dir("../client/paiement");
+
+	while (false !== ($entry = $d->read())) {
+	   
+	if( substr($entry, 0, 1) == ".") continue;
+		 $modules = new Modules();
+		 $modules->charger($entry);
+
+		 if(! $modules->id){
+			
+			$modules = new Modules();
+			$modules->nom = $entry;
+			$modules->type="1";
+			$modules->actif=0;
+			$modules->add();
+			
+		 }
+		
+		 $i++;
+	
+		if(!($i%2)) $fond="cellule_sombre";
+  		else $fond="cellule_claire";
+?>
+
    <table width="100%"  border="0" cellspacing="0" cellpadding="0">
 
-  <tr class="cellule_sombre">
-    <td width="21%" height="30">Gestion des variables</td>
-    <td width="63%" height="30">
+  <tr class="<?php echo $fond; ?>">
+    <td width="21%" height="30"><?php echo $entry; ?></td>
+    <td width="69%" height="30">
       
     </td>
     <td width="16%" height="30">
-      <div align="left"><a href="variable.php" class="txt_vert_11">Poursuivre </a><a href="variable.php"><img src="gfx/suivant.gif" width="12" height="9" border="0" /></a></div>
+      <div align="left">
+	<?php 
+		if($modules->actif){
+	?>
+		<a href="modepay_modifier.php?nom=<?php echo $entry ?>&actif=0" class="txt_vert_11">D&eacute;sactiver </a>
+	<?php
+		} else {
+	?>
+
+		<a href="modepay_modifier.php?nom=<?php echo $entry ?>&actif=1" class="txt_vert_11">Activer </a>
+		
+	<?php
+			
+		}
+	?>
+	   </div>
     </td>
-  </tr>
+  
+</tr>
+
  
   </table>
-   <table width="100%"  border="0" cellspacing="0" cellpadding="0">
 
-  <tr class="cellule_claire">
-    <td width="21%" height="30">Gestion des messages</td>
-    <td width="63%" height="30">
-      
-    </td>
-    <td width="16%" height="30">
-      <div align="left"><a href="message.php" class="txt_vert_11">Poursuivre </a><a href="message.php"><img src="gfx/suivant.gif" width="12" height="9" border="0" /></a></div>
-    </td>
-  </tr>
+<?php 
 
- 
-  </table>
+	}
+	
 
-   <table width="100%"  border="0" cellspacing="0" cellpadding="0">
+   $d->close();
+?>
 
-  <tr class="cellule_sombre">
-    <td width="21%" height="30">Gestion des plugins</td>
-    <td width="63%" height="30">
-      
-    </td>
-    <td width="16%" height="30">
-      <div align="left"><a href="plugins.php" class="txt_vert_11">Poursuivre </a><a href="plugins.php"><img src="gfx/suivant.gif" width="12" height="9" border="0" /></a></div>
-    </td>
-  </tr>
- 
-  </table>
+
 </div>
 </body>
 </html>
