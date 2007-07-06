0a7ed46890a1458a2220b7852efcec9a1298f08e
yoandm <yoandm@6d4e0a53-8826-0410-8364-8b493f4a4454>
2007-07-06 14:45:17 +0000

    thelia/client.orig/plugins/Colissimo/Colissimo.class.php
    git-svn-id: https://thelia.svn.sourceforge.net/svnroot/thelia@263 6d4e0a53-8826-0410-8364-8b493f4a4454

---
+++ thelia/client.orig/plugins/Colissimo/config.php
@@ -0,0 +1,14 @@
+<?php
+	$titre1="Colissimo";
+	$chapo1="Colissimo";
+	$description1="";
+	
+	$titre2="";
+	$chapo2="";
+	$description2="";
+	
+	$titre3="";
+	$chapo3="";
+	$description3="";
+
+?>
---
+++ thelia/client.orig/plugins/Forfait/Forfait.class.php
@@ -0,0 +1,54 @@
+<?php
+/*************************************************************************************/
+/*                                                                                   */
+/*      Thelia	                                                            		 */
+/*                                                                                   */
+/*      Copyright (c) Octolys Development		                                     */
+/*		email : thelia@octolys.fr		        	                             	 */
+/*      web : http://www.octolys.fr						   							 */
+/*                                                                                   */
+/*      This program is free software; you can redistribute it and/or modify         */
+/*      it under the terms of the GNU General Public License as published by         */
+/*      the Free Software Foundation; either version 2 of the License, or            */
+/*      (at your option) any later version.                                          */
+/*                                                                                   */
+/*      This program is distributed in the hope that it will be useful,              */
+/*      but WITHOUT ANY WARRANTY; without even the implied warranty of               */
+/*      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                */
+/*      GNU General Public License for more details.                                 */
+/*                                                                                   */
+/*      You should have received a copy of the GNU General Public License            */
+/*      along with this program; if not, write to the Free Software                  */
+/*      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA    */
+/*                                                                                   */
+/*************************************************************************************/
+?>
+<?php
+
+	include_once(realpath(dirname(__FILE__)) . "/../../../classes/PluginsTransports.class.php");
+	
+	class Forfait extends PluginsTransports{
+
+		var $poids;
+		var $nbart;
+		var $total;
+		var $zone;
+		var $pays;
+		var $unitetr;
+		var $cpostal;
+
+		function Forfait(){
+			$this->PluginsTransports;
+		}
+		
+		
+		function calcule(){
+			return $this->unitetr;
+		
+		}
+		
+	
+	}
+
+
+?>
