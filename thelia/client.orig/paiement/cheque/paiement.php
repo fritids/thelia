41d45227f7fe9b78dfb3a26003ab84f542766961
yoandm <yoandm@6d4e0a53-8826-0410-8364-8b493f4a4454>
2007-07-06 16:18:17 +0000

    thelia/client.orig/paiement
    git-svn-id: https://thelia.svn.sourceforge.net/svnroot/thelia@276 6d4e0a53-8826-0410-8364-8b493f4a4454

--- thelia/client.orig/paiement/cheque/paiement.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include_once("../../../classes/Variable.class.php");
-	$urlsite = new Variable();
-	$urlsite->charger("urlsite");
-		
-	header("Location: " . $urlsite->valeur . "/cheque.php")
-?>
