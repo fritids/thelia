b887c6ae950126b749267dd79c219d7b042b9232
yoandm <yoandm@6d4e0a53-8826-0410-8364-8b493f4a4454>
2007-07-25 15:36:59 +0000

    thelia/ChangeLog.txt
    git-svn-id: https://thelia.svn.sourceforge.net/svnroot/thelia@342 6d4e0a53-8826-0410-8364-8b493f4a4454

---
+++ thelia/client.orig/plugins/paypal/config.php
@@ -0,0 +1,34 @@
+<?php
+
+	include_once(realpath(dirname(__FILE__)) . "/../../../classes/Variable.class.php");
+
+	$titre1="Paypal";
+	$chapo1="Carte bancaire";
+	$description1="";
+	
+	$titre2="";
+	$chapo2="";
+	$description2="";
+	
+	$titre3="";
+	$chapo3="";
+	$description3="";
+
+	// Modifier la valeur ci-dessous avec l'e-mail de vote compte PayPal
+	$compte_paypal = 'mail@domaine.com';
+
+	$Devise        = "EUR";
+	$Code_Langue   = "FR";
+
+
+
+	$urlsite = new Variable();
+	$urlsite->charger("urlsite");
+	
+	$serveur="https://www.paypal.com/cgi-bin/webscr";
+    $confirm = $urlsite->valeur."/client/plugins/paypal/confirmation.php";
+	$retourok = $urlsite->valeur."/merci.php";
+	$retournok = $urlsite->valeur."/regret.php";
+	
+
+?>
