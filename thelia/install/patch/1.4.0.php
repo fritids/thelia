cf1b5f327f82a8a3735b566788d7f058fdb93bc3
raynaud_manuel <raynaud_manuel@6d4e0a53-8826-0410-8364-8b493f4a4454>
2009-01-23 13:15:59 +0000

    thelia/install/patch/1.4.0.php
    git-svn-id: https://thelia.svn.sourceforge.net/svnroot/thelia@529 6d4e0a53-8826-0410-8364-8b493f4a4454

--- thelia/install/patch/1.4.0.php
+++ thelia/install/patch/1.4.0.php
@@ -1,10 +1,26 @@
 <?php
 	include_once(realpath(dirname(__FILE__)) . "/../../classes/Cnx.class.php");
 	include_once(realpath(dirname(__FILE__)) . "/../../classes/Variable.class.php");
+	include_once(realpath(dirname(__FILE__)) . "/../../classes/Message.class.php");
+	include_once(realpath(dirname(__FILE__)) . "/../../classes/Messagedesc.class.php");
 
 	$version = new Variable();
 	$version->charger("version");
 	$version->valeur = "140";
 	$version->maj();
+	
+	$message = new Message();
+	$message->nom = 'création client';
+	$message->add();
+	
+	$message->charger('création client');
+	
+	$messagedesc = new Messagedesc();
+	$messagedesc->message = $message->id;
+	$messagedesc->lang=1;
+	$messagedesc->intitule = 'Création compte client';
+	$messagedesc->titre = 'Création compte client';
+	$messagedesc->description = 'Bonjour, Vous recevez ce mail pour vous avertir que votre compte vient d\'être crée sur __NOM_SITE__. Vos identifiants sont les suivants : e-mail : __EMAIL__ mot de passe : __MOT_DE_PASSE__ Vous pouvez modifier ces informations sur le <a href=\"__URL_SITE__\"\>site</a>';
+	$messagedesc->add();
 	
 ?>
