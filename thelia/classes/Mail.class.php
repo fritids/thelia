cf24051d095fd18a673897100ebfabf8e3eff174
raynaud_manuel <raynaud_manuel@6d4e0a53-8826-0410-8364-8b493f4a4454>
2009-03-02 12:27:28 +0000

    thelia/classes/Mail.class.php
    git-svn-id: https://thelia.svn.sourceforge.net/svnroot/thelia@550 6d4e0a53-8826-0410-8364-8b493f4a4454

---
+++ thelia/classes/Mail.class.php
@@ -0,0 +1,25 @@
+<?php
+include_once(realpath(dirname(__FILE__)) . "/../lib/phpMailer/class.phpmailer.php");
+
+ 
+class Mail extends PHPMailer{
+ 
+   function __constrcut(){
+		$this->LE = "\n";
+   }
+         
+	public function AddrFormat($addr) {
+		if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+			$formatted = $this->SecureHeader($addr[0]);
+		}
+		else{
+    		if(empty($addr[1])) {
+       			$formatted = $this->SecureHeader($addr[0]);
+     		} else {
+     			$formatted = $this->EncodeHeader($this->SecureHeader($addr[1]), 'phrase') . " <" . $this->SecureHeader($addr[0]) . ">";
+     		}
+		}
+     	return $formatted;
+   }
+ 
+}
