471d3528533388e528a7a949c460e4ad98bb0340
yoandm <yoandm@6d4e0a53-8826-0410-8364-8b493f4a4454>
2009-09-01 17:54:00 +0000

    ok
    git-svn-id: https://thelia.svn.sourceforge.net/svnroot/thelia@747 6d4e0a53-8826-0410-8364-8b493f4a4454

---
+++ thelia/fonctions/authplugin.php
@@ -0,0 +1,9 @@
+<?php
+	include_once(realpath(dirname(__FILE__)) . "/../classes/Administrateur.class.php");
+	include_once(realpath(dirname(__FILE__)) . "/../classes/Navigation.class.php");
+	
+	if(! session_id())
+		session_start();
+	if( ! isset($_SESSION["util"]->id) ) {header("Location: index.php");exit;}
+
+?>
