18db226643a99ca8390cb77190bbebc8083551da
cam.lafit <cam.lafit@azerttyu.net>
2010-02-23 22:09:22 +0100

    Coquille dans le fichier de patch
     * Pour mettre \303\240 jour il faut que le connecteur \303\240 la base de donn\303\251es soit d\303\251fini.

--- thelia/install/patch/1.4.3.php
+++ thelia/install/patch/1.4.3.php
@@ -1,6 +1,8 @@
 <?php
 	include_once(realpath(dirname(__FILE__)) . "/../../classes/Cnx.class.php");
 
+	$cnx = new Cnx();
+
 	$query_cnx = "ALTER TABLE `administrateur` ADD  `lang` INT NOT NULL ;";
 	$resul_cnx = mysql_query($query_cnx, $cnx->link);
 
