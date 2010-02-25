745d97ec32306dd52405308cc3b3bef9522fe16f
raynaud_manuel <raynaud_manuel@6d4e0a53-8826-0410-8364-8b493f4a4454>
2010-02-25 09:29:31 +0000

    admin/ajoutcli
    git-svn-id: https://thelia.svn.sourceforge.net/svnroot/thelia@830 6d4e0a53-8826-0410-8364-8b493f4a4454

--- thelia/admin/ajoutcli.php
+++ thelia/admin/ajoutcli.php
@@ -108,7 +108,7 @@ if($action == "ajouter"){
 			$mail->send();
 			?>
 				<input type="text"name="client" size="40" class="form" value="<?php echo $client->nom." ".$client->prenom; ?>">
-				<input type="hidden" name="id_client" value="<?php echo $client->ref; ?>">
+				<input type="hidden" name="id_client" id="id_client" value="<?php echo $client->ref; ?>">
 			<?php
 	}
 }
